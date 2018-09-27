f102689df9b77ef7c123d181b2317c256630c3b2
modab5310 <modab5310@7KZL6Y1.Student.UGDSB.ED>
2018-09-27 10:21:12 -0400

    Java Application

---
+++ src/wayne/gretzky/WayneGretzky.java
@@ -0,0 +1,26 @@
+/*
+* Mouner Dabjan
+* 18/9/2018
+* 
+ */
+
+package wayne.gretzky;
+
+/**
+ *
+ * @author modab5310
+ */
+public class WayneGretzky {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String playerName= "Wayne Gretzky ";
+       int number = 99;
+       System.out.println(playerName + "His number is " + number);
+               
+       
+    }
+    
+}
